8fa6f777fee3a2d9206e0589432d7818ed3daf55
DesnLee <jiakun.ui@gmail.com>
2022-01-25 17:16:04 +0800

    添加404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 Vue.use(VueRouter);
 const routes = [
     {
@@ -23,6 +24,9 @@ const routes = [
         path: '/statistics',
         name: 'Statistics',
         component: Statistics
+    }, {
+        path: '*',
+        component: NotFound
     }
 ];
 const router = new VueRouter({
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -25,6 +26,9 @@ const routes: Array<RouteConfig> = [
     path: '/statistics',
     name: 'Statistics',
     component: Statistics
+  },{
+  path: '*',
+    component: NotFound
   }
 ];
 

39b0166000c33c329b0add768010bd21d2d33bf0
DesnLee <jiakun.ui@gmail.com>
2022-01-26 13:55:37 +0800

    Nav组件添加样式

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,29 @@
   </div>
 </template>
 
-<style lang="scss">
-@import "~@/assets/styles/color.scss";
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang = "scss">
+  @import "~@/assets/styles/color.scss";
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    line-height: 1.5;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2C3E50;
+  }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class = "nav">
-    <router-link to = "/money">
+  <nav>
+    <router-link to = "/money" class = "item">
       <Icon name = "money"/>
       记账
     </router-link>
-    ｜
-    <router-link to = "/labels">
+    <router-link to = "/labels" class = "item">
       <Icon name = "labels"/>
       标签
     </router-link>
-    ｜
-    <router-link to = "/statistics">
+    <router-link to = "/statistics" class = "item">
       <Icon name = "statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang = "ts">
@@ -24,7 +22,26 @@
 </script>
 
 <style lang = "scss" scoped>
-  .nav {
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, .24);
+    flex-direction: row;
     background: #F0F0F0;
+
+    & > .item {
+      width: 33.333333%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: .5em 0;
+      font-size: 12px;
+
+      & > .icon {
+        width: 24px;
+        height: 24px;
+        margin-bottom: 4px;
+      }
+    }
   }
 </style>

c5230566755ef704c7c18ad604412824c5fee454
DesnLee <jiakun.ui@gmail.com>
2022-01-29 13:14:31 +0800

    定义 accountModel 和 tagsModel 类型

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,17 @@ interface Account {
   count: number;
   createAt?: Date;
 }
+
+interface accountModel {
+  accounts: Account[];
+  getData: () => Account[];
+  create: (data: Account) => { code: number, message: string };
+  save: () => { code: number, message: string };
+}
+
+interface tagsModel {
+  data: string[];
+  getData: () => string[];
+  create: (name: string) => { code: number, message: string };
+  save: () => { code: number, message: string };
+}

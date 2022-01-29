41612464f124518d79a27ff83c847e11611f639f
DesnLee <jiakun.ui@gmail.com>
2022-01-30 02:16:42 +0800

    完成 Button 组件

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button :style = "style">
+      <slot/>
+    </button>
+</template>
+
+<script lang = "ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import {Prop} from 'vue-property-decorator';
+
+  type Style = {
+    'background': string
+  }
+  @Component
+  export default class Button extends Vue {
+    @Prop({default: {'background': '#2E64C9'}}) style!: Style;
+  }
+</script>
+
+<style lang = "scss" scoped>
+  @import "~@/assets/styles/helper.scss";
+
+  button {
+    border: none;
+    padding: 0 16px;
+    background: $color-highlight;
+    color: #FFF;
+    line-height: 36px;
+    font-weight: bold;
+    font-size: 16px;
+    border-radius: 6px;
+  }
+</style>

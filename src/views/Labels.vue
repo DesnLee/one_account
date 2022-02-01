09e4f3c7a7b543533fe5c9b8645df330c0ba8bf3
DesnLee <jiakun.ui@gmail.com>
2022-02-01 23:58:29 +0800

    Labels.vue 添加缺省图

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,14 @@
 <template>
   <Layout>
-    <div class = "tags">
+    <div v-if = "tags.length > 0" class = "tags">
       <router-link v-for = "tag in tags" :key = "tag.id" :to = "`/labels/${tag.id}`" class = "tagItem">
         <span>{{ tag.name }}</span>
         <Icon name = "rightArrow"/>
       </router-link>
+    </div>
+    <div v-else class = "pic-wrapper">
+      <Icon class = "pic" name = "noTags"/>
+      <h2 class = "notTagsTips">还没有添加标签</h2>
     </div>
     <div class = "newTag-wrapper">
       <Button class = "newTag" @click.native = "createTag">新增标签</Button>
@@ -60,5 +64,21 @@
     text-align: center;
     padding: 24px 0;
     margin-top: 24px;
+  }
+
+  .pic {
+    width: 200px !important;
+    height: 200px !important;
+
+    &-wrapper {
+      text-align: center;
+      margin-top: 48px;
+    }
+  }
+
+  .notTagsTips {
+    text-align: center;
+    padding: 48px;
+    color: $color-second;
   }
 </style>

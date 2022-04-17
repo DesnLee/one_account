3da2a8c87997b2f17d33df4e20405a7c12aeccb7
DesnLee <jiakun.ui@gmail.com>
2022-04-17 21:17:08 +0800

    使用 Charts 组件展示收支图表

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,141 +1,165 @@
 <template>
-  <Layout class = "container">
-    <div class = "tabs-container">
-      <Tabs :data-list = "typeList" :value.sync = "typeValue" class-prefix = "type"/>
+  <Layout class="container">
+    <div class="tabs-container">
+      <Tabs :data-list="typeList" :value.sync="typeValue" class-prefix="type" />
     </div>
-    <ol v-if = "findByType.length > 0" class = "blockList">
-      <li v-for = "(group, index) in finalList" :key = "index" class = "blockItem">
-        <div class = "titleBar">
+    <Charts
+      v-show="findByType.length > 0"
+      :current-tab="currentTab"
+      :final-list="finalList"
+    />
+    <ol v-if="findByType.length > 0" class="blockList">
+      <li v-for="(group, index) in finalList" :key="index" class="blockItem">
+        <div class="titleBar">
           <h3>{{ beautifyDate(group.title) }}</h3>
           <h3>合计 ¥{{ group.total }}</h3>
         </div>
-        <ol class = "accountList">
-          <li v-for = "(item, index) in group.items" :key = "index" class = "accountItem">
-            <div class = "accountItem-top">
+        <ol class="accountList">
+          <li
+            v-for="(item, index) in group.items"
+            :key="index"
+            class="accountItem"
+          >
+            <div class="accountItem-top">
               <div>
                 <span>{{ formatTags(item.tags) }}</span>
               </div>
-              <div class = "right">
-                <span class = "cny">¥</span>
-                <span class = "count">{{ item.count }}</span>
+              <div class="right">
+                <span class="cny">¥</span>
+                <span class="count">{{ item.count }}</span>
               </div>
             </div>
-            <p v-if = "item.marks" class = "accountItem-marks">{{ item.marks }}</p>
+            <p v-if="item.marks" class="accountItem-marks">{{ item.marks }}</p>
           </li>
         </ol>
       </li>
     </ol>
-    <div v-else class = "pic-wrapper">
-      <Icon class = "pic" name = "noData"/>
-      <h2 class = "notDataTips">暂无{{ currentTab }}数据</h2>
+    <div v-else class="pic-wrapper">
+      <Icon class="pic" name="noData" />
+      <h2 class="notDataTips">暂无{{ currentTab }}数据</h2>
     </div>
   </Layout>
 </template>
 
-<script lang = "ts">
-  import {Component} from 'vue-property-decorator';
-  import Tabs from '@/components/Tabs.vue';
-  import typeList from '@/constant/typeList';
-  import dayjs from 'dayjs';
-  import deepClone from '@/lib/deepClone';
-  import InitPage from '@/mixins/InitPage';
-  import {mixins} from 'vue-class-component';
+<script lang="ts">
+  import { Component } from 'vue-property-decorator'
+  import Tabs from '@/components/Tabs.vue'
+  import Charts from '@/components/Charts.vue'
+  import typeList from '@/constant/typeList'
+  import dayjs from 'dayjs'
+  import deepClone from '@/lib/deepClone'
+  import InitPage from '@/mixins/InitPage'
+  import { mixins } from 'vue-class-component'
 
   @Component({
-    components: {Tabs}
+    components: { Tabs, Charts },
   })
   export default class Statistics extends mixins(InitPage) {
-    typeList = typeList;
-    typeValue = this.typeList[0].value;
+    typeList = typeList
+    typeValue = this.typeList[0].value
 
     get currentTab() {
-      return this.typeValue === '+' ? '收入' : '支出';
+      return this.typeValue === '+' ? '收入' : '支出'
     }
 
     get accountList() {
-      return this.$store.state.accounts.accountsData;
+      return this.$store.state.accounts.accountsData
     }
 
     get finalList() {
-      if (this.accountList.length <= 0) return this.accountList;
-      const sortedList = deepClone(this.findByType).sort((a: Account, b: Account) => dayjs(a.createAt).valueOf() - dayjs(b.createAt).valueOf());
-      const resultList: AccountTable[] = [];
+      if (this.accountList.length <= 0) return this.accountList
+      const sortedList = deepClone(this.findByType).sort(
+        (a: Account, b: Account) =>
+          dayjs(a.createAt).valueOf() - dayjs(b.createAt).valueOf()
+      )
+      const resultList: AccountTable[] = []
       for (const item of sortedList) {
-        const formatDate = dayjs(item.createAt).format('YYYY-MM-DD');
+        const formatDate = dayjs(item.createAt).format('YYYY-MM-DD')
         if (!resultList[0]) {
-          resultList.push({title: formatDate, total: item.count, items: [item]});
-          continue;
+          resultList.push({
+            title: formatDate,
+            total: item.count,
+            items: [item],
+          })
+          continue
         }
         if (resultList[0].title === formatDate) {
-          resultList[0].total += item.count;
-          resultList[0].items.unshift(item);
+          resultList[0].total += item.count
+          resultList[0].items.unshift(item)
         } else {
-          resultList.unshift({title: formatDate, total: item.count, items: [item]});
+          resultList.unshift({
+            title: formatDate,
+            total: item.count,
+            items: [item],
+          })
         }
       }
-      return resultList;
+      return resultList
     }
 
     get findByType() {
-      return this.accountList.filter((item: Account) => item.type === this.typeValue);
+      return this.accountList.filter(
+        (item: Account) => item.type === this.typeValue
+      )
     }
 
     formatTags(tags: Tag[]) {
-      return tags.length === 0 ? '无标签' : tags.map(tag => tag.name).join('，');
+      return tags.length === 0 ? '无标签' : tags.map(tag => tag.name).join('，')
     }
 
     beautifyDate(str: string) {
-      const targetDay = dayjs(str);
-      const now = dayjs();
+      const targetDay = dayjs(str)
+      const now = dayjs()
       if (now.isSame(targetDay, 'day')) {
-        return '今天';
+        return '今天'
       } else if (now.isSame(targetDay.add(1, 'day'), 'day')) {
-        return '昨天';
+        return '昨天'
       } else if (now.isSame(targetDay.add(2, 'day'), 'day')) {
-        return '前天';
+        return '前天'
       } else if (now.isSame(targetDay, 'year')) {
-        return targetDay.format('M月D日');
+        return targetDay.format('M月D日')
       } else {
-        return targetDay.format('YYYY年M月D日');
+        return targetDay.format('YYYY年M月D日')
       }
     }
 
     beautifyNumber(num: number) {
-      console.log(num);
-      const [int, float] = num.toString().split('.');
-      const afterInt = int.replace(/(\d{1,3})(?=(\d{3})+$)/g, '$1,');
-      return float ? afterInt + '.' + float : afterInt;
+      console.log(num)
+      const [int, float] = num.toString().split('.')
+      const afterInt = int.replace(/(\d{1,3})(?=(\d{3})+$)/g, '$1,')
+      return float ? afterInt + '.' + float : afterInt
     }
   }
 </script>
 
-<style lang = "scss" scoped>
-  @import "~@/assets/styles/helper.scss";
+<style lang="scss" scoped>
+  @import '~@/assets/styles/helper.scss';
 
   .container {
-    background: #F4F4F4;
-
+    background: #f4f4f4;
   }
 
   .tabs-container {
     @extend %outerShadow;
     position: sticky;
+    background: #ffffff;
     top: 0;
+    z-index: 10;
   }
 
   ::v-deep {
     .type-tabs {
       color: $color-third;
-      background: #F8F8F8;
+      background: #f8f8f8;
 
       > .type-tabs-item.selected {
         color: $color-highlight;
-        background: #FFF;
+        background: #fff;
       }
     }
 
     .interval-tabs {
-      background: #F8F8F8;
+      background: #f8f8f8;
       color: $color-second;
 
       > .interval-tabs-item {
@@ -147,14 +171,13 @@
         &.selected {
           color: $color-highlight;
           font-weight: bold;
-          background: #F8F8F8;
+          background: #f8f8f8;
         }
       }
     }
   }
 
   .blockList {
-
     > .blockItem {
       margin-top: 8px;
 
@@ -172,11 +195,11 @@
       }
 
       > .accountList {
-        background: #FFF;
+        background: #fff;
         padding: 0 16px;
 
         > .accountItem {
-          border-bottom: 1px solid #F4F4F4;
+          border-bottom: 1px solid #f4f4f4;
           padding: 16px 0;
 
           > .accountItem-top {
@@ -210,7 +233,6 @@
             font-size: 12px;
             margin-top: 4px;
           }
-
         }
       }
     }
